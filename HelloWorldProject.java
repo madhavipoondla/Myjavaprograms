38235f83b41adc67cd1d14a062c95657e98df68a
MADHAVI LATHA POONDLA <120589713+madhavipoondla@users.noreply.github.com>
2025-06-21 18:04:40 +0530

    Add HelloWorld.java

---
+++ HelloWorldProject.java
@@ -0,0 +1,10 @@
+// This is the main class. in java everything runs inside a class.
+public class HelloWorld {
+	
+// This is the main method. It's the entry point of any java application.
+	public static void main(String[] args) {
+		
+//This line prints "Hello ,Let's code!" to the console.
+		System.out.println("Hello, Let's code!");
+	}
+}
